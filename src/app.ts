fb336acc33828dba074e6304a50282de39a3ba82
Hariksh Suryawanshi <hariksh2007@gmail.com>
2026-02-02 08:12:50 +0530

    Change database connection string to empty
    Update database connection string to an empty string.

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ export default class App implements App_Interface{
     };
     async connectDatabase():Promise<void>{
         try{
-            await mongoose.connect("mongodb+srv://hariksh2007_db_user:Hariksh%4007@cluster0.704vsvr.mongodb.net/todo_app?retryWrites=true&w=majority")
+            await mongoose.connect("")
             console.log('database connected')
         }
         catch(err){
